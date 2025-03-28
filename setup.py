262047fa27c10750250503c048c01454b3fe0538
Pankaj Mishra <pankajmisr@gmail.com>
2025-03-28 01:36:45 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""
+Setup script for the github_react_agent package.
+This setup script allows for pip installation using:
+    pip install -e .
+"""
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
