acdea8b25dec32172c046d600c3335fcbf510146
Pankaj Mishra <pankajmisr@gmail.com>
2025-03-28 01:36:59 -0400

    Add github_react_agent/__init__.py

---
+++ github_react_agent/__init__.py
@@ -0,0 +1,9 @@
+"""
+GitHub ReAct Agent - A ReAct agent for interacting with GitHub repositories.
+"""
+
+__version__ = "0.1.0"
+
+from github_react_agent.agent import create_agent
+
+__all__ = ["create_agent"]
