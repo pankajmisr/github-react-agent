c5642bd6236d32dadb80359914dc1ec4a5fa5de7
Pankaj Mishra <pankajmisr@gmail.com>
2025-03-31 23:01:00 -0400

    Add file metadata tool to the tools package

--- github_react_agent/tools/__init__.py
+++ github_react_agent/tools/__init__.py
@@ -6,6 +6,7 @@ from github_react_agent.tools.base import GitHubBaseTool, GitHubToolException
 from github_react_agent.tools.branch import GitHubCreateBranchTool, GitHubListBranchesTool
 from github_react_agent.tools.commit import GitHubCommitFileTool, GitHubCommitMultipleFilesTool
 from github_react_agent.tools.file_content import GitHubGetFileContentTool
+from github_react_agent.tools.file_metadata import GitHubGetFileMetadataTool
 from github_react_agent.tools.pull_request import GitHubCreatePullRequestTool
 from github_react_agent.tools.repo_contents import GitHubListContentsTool
 from github_react_agent.tools.repo_details import GitHubRepoDetailsTool
@@ -18,6 +19,7 @@ __all__ = [
     "GitHubRepoDetailsTool",
     "GitHubListContentsTool",
     "GitHubGetFileContentTool",
+    "GitHubGetFileMetadataTool",
     "GitHubCreateBranchTool",
     "GitHubListBranchesTool",
     "GitHubCommitFileTool",
@@ -34,6 +36,7 @@ def get_github_tools():
         GitHubRepoDetailsTool(),
         GitHubListContentsTool(),
         GitHubGetFileContentTool(),
+        GitHubGetFileMetadataTool(),
         GitHubCreateBranchTool(),
         GitHubListBranchesTool(),
         GitHubCommitFileTool(),
